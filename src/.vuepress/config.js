fc2dd63caa92383ba920748681c39774b17847a4
Yankue <yankuedevco@gmail.com>
2022-07-29 11:39:15 +0100

    another fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,8 +8,6 @@ const config = {
 
 	theme: 'yuu',
 
-	base: '/guide',
-
 	head: [
 		['meta', { charset: 'utf-8' }],
 		['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
